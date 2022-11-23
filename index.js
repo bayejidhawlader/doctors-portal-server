310c68e945cff76921a0b93065f565b244e857ba
Bayejid Hawlader <bayejidhawlader@gmail.com>
2022-11-23 16:32:34 +0600

    Complete Module 76

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+// 17, 18(255), 19, 20(Stripe), 21(Stripe, 221) 22(Payment Collection 74, 242 (checkoutForm 74) )
+
 // 01 Basic Setup
 const express = require("express");
+require("dotenv").config();
 
 // 10 Json Web Token
 const jwt = require("jsonwebtoken");
 
+// 20 Require STRIPE key
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 // 02.1 Connect Mongo Db Application to Server
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require("dotenv").config();
 
 // 01.0 Basic Setup
 const cors = require("cors");
@@ -31,11 +36,13 @@ function verifyJwt(req, res, next) {
   if (!authHeader) {
     return res.status(401).send("UnAuthorized Access");
   }
-
-  const token = authHeader.split("")[1];
+  console.log(authHeader);
+  const token = authHeader.split(" ")[1];
+  // console.log(token);
 
   // 13 Verify Token
   jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    console.log(err, decoded);
     if (err) {
       return res.status(403).send({ message: "Forbidden Access" });
     }
@@ -61,6 +68,28 @@ async function run() {
       .db("doctorsPortal")
       .collection("bookings");
     const usersCollection = client.db("doctorsPortal").collection("users");
+
+    // 15 for Doctors Collection http://localhost:3000/dashboard/adddoctor
+    const doctorsCollection = client.db("doctorsPortal").collection("doctors");
+
+    // 22 Collection for Payments ('/' 282)
+    const paymentsCollection = client
+      .db("doctorsPortal")
+      .collection("payments");
+
+    // 18 Verify Admin after Deleted Admin Manage Doctors Page (Make sure you use verifyAdmin agter verifyJWT)
+    const verifyAdmin = async (req, res, next) => {
+      // console.log("Inside very Admin", req.decoded.email);
+      const decoded = req.decoded.email;
+      const query = { email: decoded };
+      const user = await usersCollection.findOne(query);
+
+      if (user.role !== "admin") {
+        return res.status(403).send({ message: "forbidenn access" });
+      }
+      // res.decoded = req.decoded;
+      next();
+    };
 
     // Use AgreeGet to query t miltiple collection and then merge data
     app.get("/AppoinmentOptions", async (req, res) => {
@@ -140,6 +169,16 @@ async function run() {
       res.send(options);
     });
 
+    // 15 http://localhost:3000/dashboard/adddoctor add a doctor Speciality Dropdown
+    app.get("/appoinmentSpecialty", async (req, res) => {
+      const query = {};
+      const result = await appoinmentOptionCollection
+        .find(query)
+        .project({ name: 1 })
+        .toArray();
+      res.send(result);
+    });
+
     // 08 My Appointment showing on Dashboard Page
     app.get("/bookings", verifyJwt, async (req, res) => {
       const email = req.query.email;
@@ -153,6 +192,14 @@ async function run() {
       const query = { email: email };
       const bookings = await bookingsCollection.find(query).toArray();
       res.send(bookings);
+    });
+
+    // 19 for Payment page with Dynamic id http://localhost:3000/dashboard/payment
+    app.get("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const booking = await bookingsCollection.findOne(query);
+      res.send(booking);
     });
 
     // 04.0 Appoinment Modal sumbit mongodb database received
@@ -176,6 +223,60 @@ async function run() {
       res.send(result);
     });
 
+    // 21 Stripe Payment create-payment-intent (CheckoutForm 10)
+    app.post("/create-payment-intent", async (req, res) => {
+      const booking = req.body;
+      const price = booking.price;
+      const amount = price * 100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "usd",
+        amount: amount,
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    // 22 Collection for Payments
+    // app.post("/payments", async (req, res) => {
+    //   const payment = req.body;
+    //   const result = await paymentsCollection.insertOne(payment);
+    //   const _id = payment.bookingId;
+    //   const flter = { _id: ObjectId(id) };
+    //   const updatedDoc = {
+    //     $set: {
+    //       paid: true,
+    //       transactionId: payment.transactionId,
+    //     },
+    //   };
+    //   const updatedResult = await bookingsCollection.updateOne(
+    //     filter,
+    //     updatedDoc
+    //   );
+    //   res.send(result);
+    // });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const result = await paymentsCollection.insertOne(payment);
+      const id = payment.bookingId;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+      const updatedResult = await bookingsCollection.updateOne(
+        filter,
+        updatedDoc
+      );
+      console.log(result);
+      res.send(result);
+    });
+
     // 11 Create new jwt token for login with google
     app.get("/jwt", async (req, res) => {
       const email = req.query.email;
@@ -191,7 +292,7 @@ async function run() {
       res.status(403).send({ accessToken: "" });
     });
 
-    // 13 Check user they are Admin or not
+    // 14 Check user they are Admin or not
     app.get("/users/admin/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email };
@@ -207,15 +308,7 @@ async function run() {
     });
 
     // 13 Make user Admin
-    app.put("/users/admin/:id", verifyJwt, async (req, res) => {
-      const decoded = req.decoded.email;
-      const query = { email: decodedEmail };
-      const user = await usersCollection.findOne(query);
-
-      if (user.role !== "admin") {
-        return res.status(403).send({ message: "forbidenn access" });
-      }
-
+    app.put("/users/admin/:id", verifyJwt, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
       const options = { upsert: true };
@@ -232,11 +325,50 @@ async function run() {
       res.send(result);
     });
 
+    // 18 Temporary to update price field on appoinment option
+    // app.get("/addPrice", async (req, res) => {
+    //   const filter = {};
+    //   const options = { upsert: true };
+    //   const updatedDoc = {
+    //     $set: {
+    //       price: 99,
+    //     },
+    //   };
+    //   const result = await appoinmentOptionCollection.updateMany(
+    //     filter,
+    //     updatedDoc,
+    //     options
+    //   );
+    //   res.send(result);
+    // });
+
     // 09
     app.post("/users", async (req, res) => {
       const user = req.body;
       // console.log(user);
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // 16 Show all Doctors to ManageDoctors Page http://localhost:3000/dashboard/managedoctors
+    app.get("/doctors", verifyJwt, async (req, res) => {
+      const query = {};
+      const doctors = await doctorsCollection.find(query).toArray();
+      res.send(doctors);
+    });
+
+    // 15.0 for Doctors Collection
+    app.post("/doctors", verifyJwt, verifyAdmin, async (req, res) => {
+      const doctor = req.body;
+      const result = await doctorsCollection.insertOne(doctor);
+      res.send(result);
+    });
+
+    // 17 Manage Doctor Page, Doctor Delete
+    app.delete("/doctors/:id", verifyJwt, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await doctorsCollection.deleteOne(filter);
       res.send(result);
     });
   } finally {
